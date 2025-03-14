0bbf15f92af04b7a6863721667053d50f1ce0eff
jaysh-rob <jasonroys210@gmail.com>
2025-03-14 22:52:35 +0000

    quize game completed and guess numbers added

---
+++ number_guesser.py
@@ -0,0 +1,40 @@
+import random
+
+random_number_user = input("Enter the number you want to guess: ")
+
+if random_number_user.isdigit():
+    random_number_user = int(random_number_user)
+
+    if random_number_user <= 0:
+        print("Please enter a number greater than 0")
+        quit()
+else:
+    print("Please enter a number")
+    quit()
+
+random_number = random.randint(0, random_number_user)
+
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+    if user_guess == random_number:
+        print("You got it")
+        break
+    else:
+        if user_guess > random_number:
+            print("Your number was greater")
+        else:
+            print("Your number was less")
+print("You got it in ", guesses, "guesses")
+
+
+
+    
+
--- quiz_game.py
+++ quiz_game.py
@@ -5,32 +5,40 @@ if playing.lower() != "yes":
         quit()
 
 print("Okay! Let's Play")
-
+score = 0
 
 answer = input("What does CPU stands for? ").lower()
 
 if answer == "central processing unit":
         print("Correct")
+        score += 1
 else:
         print("Incorrect")
 
 answer = input("What does GPU stand for? ").lower()
 if answer == "graphics processing unit":
         print("Correct")
+        score += 1
 else:
         print("Incorrect")
 
 answer = input("What does RAM stands for? ")
 if answer.lower() == "Random access memory":
         print("Correct")
+        score += 1
 else:
         print("Incorrect")
 
 answer = input("What does PSU stands for?")
 if answer.lower() == "power supply":
+        score += 1 
         print("Correct")
 else:
         print("Incorrect")
+
+print(f"You got total of {score} questions correct.")
+print("You got total of" + str(score) + "questions correct.")
+print("You got total of" + " " + str((score / 4 ) * 100) + "%." + "questions correct.")
 
 
 
